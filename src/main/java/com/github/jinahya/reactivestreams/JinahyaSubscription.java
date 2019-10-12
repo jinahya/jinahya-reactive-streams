756c95a19f5abc32883e9fe76e608c45a2f39c3f
Jin Kwon <onacit@gmail.com>
2019-10-12 17:28:49 +0900

    Add JinahyaSubscription

---
+++ src/main/java/com/github/jinahya/reactivestreams/JinahyaSubscription.java
@@ -0,0 +1,49 @@
+package com.github.jinahya.reactivestreams;
+
+import org.reactivestreams.Subscription;
+
+import java.util.function.Consumer;
+import java.util.function.LongConsumer;
+
+import static java.util.Objects.requireNonNull;
+
+public class JinahyaSubscription implements Subscription {
+
+    // -----------------------------------------------------------------------------------------------------------------
+    public JinahyaSubscription(final LongConsumer requestConsumer, final Consumer<Void> cancellationConsumer) {
+        super();
+        if (requestConsumer == null) {
+            throw new NullPointerException("requestConsumer is null");
+        }
+        if (cancellationConsumer == null) {
+            throw new NullPointerException("cancellationConsumer is null");
+        }
+        this.requestConsumer = requireNonNull(requestConsumer, "requestConsumer is null")
+        this.cancellationConsumer = v -> {
+            if (cancelled) {
+                throw new IllegalStateException("already cancelled");
+            }
+            cancelled = true;
+            cancellationConsumer.accept(null);
+        };
+    }
+
+    // -----------------------------------------------------------------------------------------------------------------
+    @Override
+    public void request(final long n) {
+        requestConsumer.accept(n);
+    }
+
+    @Override
+    public void cancel() {
+        cancellationConsumer.accept(null);
+    }
+
+    // -----------------------------------------------------------------------------------------------------------------
+    private final LongConsumer requestConsumer;
+
+    // -----------------------------------------------------------------------------------------------------------------
+    private transient boolean cancelled;
+
+    private final Consumer<Void> cancellationConsumer;
+}
